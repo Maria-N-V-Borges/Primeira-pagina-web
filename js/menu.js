641ccc5dee821a524b0cd3ec5837d10eb8919d90
Maria N. V. Borges <m.nazare.borges.cc@gmail.com>
2025-12-29 16:01:17 -0300

    Create menu.js
    Signed-off-by: Maria N. V. Borges <m.nazare.borges.cc@gmail.com>

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+	const menu = document.getElementById("menuLinks");
+	menu.classList.toggle("ativo");
+}
+
+// clasList.toggle("ativo") significa:
+// se tiver ➡️ remove
+// se não tiver ➡️ adiciona
