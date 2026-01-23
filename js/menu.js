c4ce4b761758b508017993e0a8ef4e8979848050
Maria N. V. Borges <m.nazare.borges.cc@gmail.com>
2026-01-23 13:40:45 -0300

    Update menu.js
    Signed-off-by: Maria N. V. Borges <m.nazare.borges.cc@gmail.com>

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,10 @@
+// Função para abrir/fechar o menu hambúrguer no mobile
 function toggleMenu() {
 	const menu = document.getElementById("menuLinks");
 	menu.classList.toggle("ativo");
 }
 
+//Função para ativar/desativar o Dark Mode
 function toggleDarkMode() {
 	const body = document.body;
 	const header = document.querySelector("header");
@@ -14,6 +16,7 @@ function toggleDarkMode() {
 	const buttons = document.querySelectorAll("button");
 	const toggleBtn = document.getElementById("modoToggle");
 
+	// Alterna a classe "dark-mode" em vários elementos
 	body.classList.toggle("dark-mode");
 	header.classList.toggle("dark-mode");
 	footer.classList.toggle("dark-mode");
@@ -23,8 +26,39 @@ function toggleDarkMode() {
 	buttons.forEach(el => el.classList.toggle("dark-mode"));
 	menuLinks.forEach(el => el.classList.toggle("dark-mode"));
 
-	// Troca o ícone do botão
-	toggleBtn.textContent = body.classList.contains("dark-mode")
-		? "☀️ Light Mode"
-		: "🌙 Dark Mode";
-}
+	// Verifica se está em dark mode
+	const isDark = body.classList.contains("dark-mode");
+
+	// Troca o texto do botão
+	toggleBtn.textContent = isDark ? "☀️ Light Mode" : "🌙 Dark Mode";
+
+	// Salva a preferência no navegador
+	localStorage.setItem("darkMode", isDark ? "enabled" : "disabled");
+	}
+
+// Quando a página carrega, aplica o Dark Mode se estiver salvo
+window.addEventListener("DOMContentLoaded", () => {
+	const darMode = localStorage.getItem("darkMode");
+	if (darMode === "enabled") {
+		const body = document.body;
+		const header = document.querySelector("header");
+		const footer = document.querySelector("footer");
+		const titles = document.querySelectorAll("h2, h3");
+		const menu = document.querySelector(".menu");
+		const menuLinks = document.querySelectorAll(".menu a");
+		const links = document.querySelectorAll("a");
+		const buttons = document.querySelectorAll("button");
+		const toggleBtn = document.getElementById("modoToggle");
+
+		body.classList.toggle("dark-mode");
+		header.classList.toggle("dark-mode");
+		footer.classList.toggle("dark-mode");
+		menu.classList.toggle("dark-mode");
+		titles.forEach(el => el.classList.toggle("dark-mode"));
+		links.forEach(el => el.classList.toggle("dark-mode"));
+		buttons.forEach(el => el.classList.toggle("dark-mode"));
+		menuLinks.forEach(el => el.classList.toggle("dark-mode"));
+		
+		toggleBtn.textContent = "☀️ Light Mode";
+	}
+});
